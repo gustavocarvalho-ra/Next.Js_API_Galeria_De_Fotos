b7dd1871fedecae259db6e97fdc4477658a01b4a
GustavoCR021 <gustavoc1922@gmail.com>
2025-01-28 22:16:36 -0300

    style: changed color root

--- src/app/components/Header/styles.ts
+++ src/app/components/Header/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const Head = styled.div `
   width: 100%;
   height: 6vh;
-  background-color: #161616;
+  background-color: var(--second-background);
   display: flex;
   align-items: center;
 
