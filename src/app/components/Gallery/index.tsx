e19eb4527b6953666671e6f109c5cd9c39e642f9
GustavoCR021 <gustavoc1922@gmail.com>
2025-01-29 22:46:44 -0300

    feat: test component

---
+++ src/app/components/Gallery/index.tsx
@@ -0,0 +1,10 @@
+import { Gall } from "./styles";
+
+export default function Gallery() {
+  return (
+    <Gall>
+      <h2>Test galeria</h2>
+
+    </Gall>
+  )
+}
---
+++ src/app/components/Gallery/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Gall = styled.div `
+  width: 100%;
+  height: 70%;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
