3e3877a02de1fdcae9d27dcd71561f2320e64a34
GustavoCR021 <gustavoc1922@gmail.com>
2025-01-29 22:50:06 -0300

    style: changed height and add margin

--- src/app/components/Gallery/styles.ts
+++ src/app/components/Gallery/styles.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components"
 
 export const Gall = styled.div `
-  width: 100%;
-  height: 70%;
+  width: 90%;
+  height: 60vh;
   background-color: red;
   display: flex;
   align-items: center;
   justify-content: center;
+  margin: 5em;
 `
