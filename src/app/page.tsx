8d123deae83e5a39ae8ad19f1b322c95885da7c2
GustavoCR021 <gustavoc1922@gmail.com>
2025-01-29 22:47:43 -0300

    feat: add component in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 
 import Header from "./components/Header";
+import Gallery from "./components/Gallery";
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Gallery />
+    </>
   );
 }
