95bf0f4fa21ef6f471b9aa00b63899c66ffc2e57
GustavoCR021 <gustavoc1922@gmail.com>
2025-02-03 23:37:03 -0300

    feat: test component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@
 
 import Header from "./components/Header";
 import Gallery from "./components/Gallery";
-
+import CartImage from "./components/CartImages";
 export default function Home() {
   return (
     <>
       <Header />
       <Gallery />
+      <CartImage />
     </>
   );
 }
