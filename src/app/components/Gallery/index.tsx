450bba5cda48f8dec0588aeaadb909c75a567cfd
GustavoCR021 <gustavoc1922@gmail.com>
2025-02-06 14:41:25 -0300

    feat: test api images

--- src/app/components/Gallery/index.tsx
+++ src/app/components/Gallery/index.tsx
@@ -1,9 +1,38 @@
+"use client"
+import { useState, useEffect} from "react";
+
 import { Gall } from "./styles";
+import Image from "next/image";
 
 export default function Gallery() {
+  const [images, setImages] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchImages() {
+      const response = await fetch("/api/unsplash?query=technology");
+      const data = await response.json();
+      setImages(data);
+      setLoading(false);
+    }
+
+    fetchImages();
+  }, []);
+
+  if (loading) return <p>Carregando..</p>;
+
   return (
     <Gall>
-      <h2>Test galeria</h2>
+      {images.map((image) => (
+        <Image
+          width={700}
+          height={500}
+          key={image.id}
+          src={image.urls.small}
+          alt={image.alt_description}
+          className="rounded-lg shadow-md w-full h-auto"
+        />
+      ))}
 
     </Gall>
   )
