1ad390acf7e72ef46f8e55e98697c6d33cbe6fa1
GustavoCR021 <gustavoc1922@gmail.com>
2025-02-06 21:56:03 -0300

    fix: ajusted nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
+    // domains: ["images.unsplash.com"],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        // pathname: '/account123/**',
-        search: '',
       },
     ],
   },
