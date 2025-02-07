ac77f099a06ff730a7617c4ebdb5e353ac9ca856
GustavoCR021 <gustavoc1922@gmail.com>
2025-02-07 14:36:50 -0300

    feat: added all image sizes

--- src/app/types/imag.ts
+++ src/app/types/imag.ts
@@ -1,5 +1,12 @@
 export interface imageprops{
     id: string;
     alt_description: string;
-    urls: {small: string};
+    urls: {
+      full: string,
+      raw: string,
+      regular: string,
+      small: string,
+      small_s3: string,
+      thumb: string
+    };
   }
