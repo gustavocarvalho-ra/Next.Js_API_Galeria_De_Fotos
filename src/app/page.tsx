5cb3d95d2f779620e32692dfd39c0f778ff5e260
GustavoCR021 <gustavoc1922@gmail.com>
2025-02-15 23:31:18 -0300

    feat: import function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@
 import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 import CartImage from "./components/CartImages";
+import Nav from "./components/Nav";
 export default function Home() {
   return (
     <>
       <Header />
+      <Nav />
       <Gallery />
       <CartImage />
     </>
