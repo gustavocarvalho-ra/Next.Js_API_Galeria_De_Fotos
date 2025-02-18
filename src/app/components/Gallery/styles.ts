5bbb4220bd9879d3590164af25d537ded2fed782
GustavoCR021 <gustavoc1922@gmail.com>
2025-02-18 00:02:24 -0300

    style: ajust gap

--- src/app/components/Gallery/styles.ts
+++ src/app/components/Gallery/styles.ts
@@ -3,11 +3,11 @@ import styled from "styled-components"
 export const Gall = styled.div `
   width: 95%;
   height: 150vh;
-  background-color: red;
+  /* background-color: red; */
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   margin: 5em 5em 4em 5em;
-  gap: 1em;
+  gap: 1.8em;
 `
