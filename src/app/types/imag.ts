7a0bc4252c18d0f1d8ac14889e130e476568570d
GustavoCR021 <gustavoc1922@gmail.com>
2025-02-19 22:39:47 -0300

    feat: add new interface

--- src/app/types/imag.ts
+++ src/app/types/imag.ts
@@ -1,12 +1,16 @@
-export interface imageprops{
-    id: string;
-    alt_description: string;
-    urls: {
-      full: string,
-      raw: string,
-      regular: string,
-      small: string,
-      small_s3: string,
-      thumb: string
-    };
-  }
+export interface imageprops {
+  id: string;
+  alt_description: string;
+  urls: {
+    full: string,
+    raw: string,
+    regular: string,
+    small: string,
+    small_s3: string,
+    thumb: string
+  };
+}
+
+export interface ImageDivProps {
+  imageUrl: string;
+}
