b1fa1e61f4850b39b37040491b03284d290973d8
GustavoCR021 <gustavoc1922@gmail.com>
2025-03-13 22:52:00 -0300

    feat: navigation bar change

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import { NavBar } from "./styles";
 
 export default function Nav() {
   return (
     <NavBar>
-      <h1>test nav</h1>
+      <Link href={"/"}>test nav</Link>
 
     </NavBar>
   )
