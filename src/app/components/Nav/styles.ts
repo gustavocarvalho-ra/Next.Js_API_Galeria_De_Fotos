c7d4c288c9737916973c1f01c788bc552787eb5b
GustavoCR021 <gustavoc1922@gmail.com>
2025-03-15 23:48:13 -0300

    style: changed background color

--- src/app/components/Nav/styles.ts
+++ src/app/components/Nav/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const NavBar = styled.div `
   width: 100%;
   height: 4vh;
-  background: purple;
+  background-color: var(--second-background);
   display: flex;
   align-items: center;
 `
