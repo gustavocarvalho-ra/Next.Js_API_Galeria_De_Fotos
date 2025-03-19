c1557088cf46c77c5956b9c269773ef8634b409b
GustavoCR021 <gustavoc1922@gmail.com>
2025-03-18 23:37:57 -0300

    feat: test component

--- src/app/ty/wallpapers/page.tsx
+++ src/app/ty/wallpapers/page.tsx
@@ -1,42 +1,5 @@
-"use client";
+import { Gallery } from './../../components/Gallery/index';
 
-import Link from "next/link";
-import { Gall } from "../styles";
-import { useImages } from "../../hooks/useImages";
-import { ImageDiv } from "../../components/ImageDiv";
-import { useMemo } from "react";
-
-const NUM_COLUMNS = 3;
-
-export default function Galleryy({ imageType = "wallpaper" }) {
-  const { images = [], loading, error } = useImages(imageType);
-
-  const columns = useMemo(() => {
-    const cols: typeof images[] = Array.from({ length: NUM_COLUMNS }, () => []);
-    images.forEach((image, index) => {
-      cols[index % NUM_COLUMNS].push(image);
-    });
-    return cols;
-  }, [images]);
-
-  if (loading) return <p>Carregando...</p>;
-  if (error) return <p>{error}</p>;
-
-  return (
-    <Gall>
-      <div className="windowGallery">
-        {columns.map((col, colIndex) => (
-          <div className="cln" key={colIndex}>
-            {col.map((image) => (
-              <div className="image" key={image.id}>
-                <Link href={image.urls.full} target="_blank">
-                  <ImageDiv imageUrl={image.urls.small} />
-                </Link>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </Gall>
-  );
+export default function WallpapersPage() {
+  return <Gallery imageType="wallpaper" />;
 }
