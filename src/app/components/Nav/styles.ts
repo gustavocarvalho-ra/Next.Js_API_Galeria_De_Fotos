7cf65743ee22eb642fa2bc07e3b716b72172436b
GustavoCR021 <gustavoc1922@gmail.com>
2025-03-20 20:30:05 -0300

    style: add ::after style

--- src/app/components/Nav/styles.ts
+++ src/app/components/Nav/styles.ts
@@ -10,16 +10,36 @@ export const NavBar = styled.div `
   gap: 1em;
 
   a {
+    height: 100%;
     text-decoration: none;
     font-weight: 800;
     font-size: .83em;
     color: var(--text-grey-sec);
     letter-spacing: 1px;
+    position: relative;
+    align-content: center;
 
     &:hover {
-      color: #C0C0C0;
+      color: var(--text-grey-ter);
     }
 
-    
+    &::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: -2px;
+      width: 100%;
+      height: 1px;
+      background: var(--text-grey-ter);
+      transform: scaleX(0);
+      transform-origin: right;
+      transition: transform .4s;
+    }
+
+    &:hover::after {
+      transform: scaleX(1);
+      transform-origin: left;
+      transition: transform .4s;
+    } 
   }
 `
