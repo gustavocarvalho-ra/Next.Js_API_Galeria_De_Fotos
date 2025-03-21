5970e4b7ff51560ea2374d452f13da91ad74ff8a
GustavoCR021 <gustavoc1922@gmail.com>
2025-03-21 11:27:11 -0300

    feat: add new link page

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -4,6 +4,7 @@ import { NavBar } from "./styles";
 export default function Nav() {
   return (
     <NavBar>
+      <Link href={"../../ty/foto"}>Foto</Link>
       <Link href={"../../ty/wallpapers"}>Wallpaper</Link>
       <Link href={"../../ty/textura"}>Textura</Link>
       <Link href={"../../ty/universo"}>Universo</Link>
