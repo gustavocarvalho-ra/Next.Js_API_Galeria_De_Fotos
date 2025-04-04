8327e19ca82fe6b3d12dafb97827b66715860430
GustavoCR021 <gustavoc1922@gmail.com>
2025-04-03 23:10:03 -0300

    style: changed tag

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,14 +1,20 @@
 import { Foot } from "./styles";
 
+import Link from 'next/link';
 import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <Foot>
-      <h1>test cart</h1>
+      <h2>Obrigado pela visita!</h2>
       <div className="icons">
-        <FaGithub />
-        <FaLinkedin />
+        <Link href="https://github.com/gustavocarvalho-ra" target="_blank">
+          <FaGithub />
+        </Link>
+
+        <Link href="https://www.linkedin.com/in/gustavo-carvalho-7bb361205/" target="_blank">
+          <FaLinkedin />
+        </Link>
       </div>
     </Foot>
   )
