5778f8cdc6c8f7e51d9470b5855613e755eb043f
GustavoCR021 <gustavoc1922@gmail.com>
2025-04-03 23:12:42 -0300

    style: changed style of footer

--- src/app/components/Footer/styles.ts
+++ src/app/components/Footer/styles.ts
@@ -3,5 +3,18 @@ import styled from "styled-components"
 export const Foot = styled.div `
   width: 100%;
   height: 5vh;
-  background-color: green;
+  background-color: var(--second-background);
+  align-content: center;
+
+  h2 {
+    font-size: 1.2em;
+    color: var(--text-grey-ter),
+  }
+
+  .icons {
+    display: flex;
+    a {
+      color: var(--text-grey-ter),
+    }
+  }
 `
