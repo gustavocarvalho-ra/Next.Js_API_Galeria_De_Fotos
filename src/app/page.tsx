24cf93cf1fe168a20a928085ebe6878036f6e0dd
GustavoCR021 <gustavoc1922@gmail.com>
2025-04-08 23:28:48 -0300

    feat: deleted footer in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import CartImage from "./components/Footer";
 import { Gallery } from "./components/Gallery";
 
 export default function Home() {
   return (
     <>
       <Gallery imageType="random"/>
-      <CartImage />
     </>
   );
 }
