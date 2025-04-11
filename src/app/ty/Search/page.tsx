22c2720d5a006f5acf4ec5286e65ce53bfed0622
GustavoCR021 <gustavoc1922@gmail.com>
2025-04-11 20:59:47 -0300

    feat: add import's

--- src/app/ty/Search/page.tsx
+++ src/app/ty/Search/page.tsx
@@ -2,15 +2,23 @@
 
 import { useSearchParams } from "next/navigation";
 import ImageResults from "@/app/components/ImageResults";
+import Header from "@/app/components/Header";
+import Nav from "@/app/components/Nav";
+import Footer from "@/app/components/Footer";
 
 export default function SearchPage() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "nature";
 
   return (
-    <div>
-      <h2>Resultados para: &quot;{query}&quot;</h2>
-      <ImageResults query={query} />
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <div>
+        <h2>Resultados para: &quot;{query}&quot;</h2>
+        <ImageResults query={query} />
+      </div>
+      <Footer />
+    </>
   );
 }
