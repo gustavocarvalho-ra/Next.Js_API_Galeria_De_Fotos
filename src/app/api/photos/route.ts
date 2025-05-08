f2d271ee748318c31981510adb7e7c2999dc76ff
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-08 00:15:34 -0300

    feat: test route

--- src/app/api/photos/route.ts
+++ src/app/api/photos/route.ts
@@ -1,29 +1,23 @@
-// import { NextResponse } from "next/server";
-// import { unsplash } from "@/app/lib/unsplash";
-
-// export async function GET(req: Request) {
-//   const { searchParams } = new URL(req.url);
-//   const query = searchParams.get("query") || "nature";
-
-//   try {
-//     const result = await unsplash.search.getPhotos({
-//       query,
-//       perPage: 30,
-//     });
-
-//     if (result.errors) {
-//       return NextResponse.json({ error: result.errors }, { status: 400});
-//     }
-
-//     return NextResponse.json(result.response.results);
-//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//   } catch (error) {
-//     return NextResponse.json({ error: "Erro ao buscar imagens" }, { status: 500 });
-//   }
-// }
-
 import { NextResponse } from "next/server";
+import { unsplash } from "@/app/lib/unsplash";
 
-export async function GET() {
-  return NextResponse.json({ ok: true });
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const query = searchParams.get("query") || "nature";
+
+  try {
+    const result = await unsplash.search.getPhotos({
+      query,
+      perPage: 30,
+    });
+
+    if (result.errors) {
+      return NextResponse.json({ error: result.errors }, { status: 400});
+    }
+
+    return NextResponse.json(result.response.results);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return NextResponse.json({ error: "Erro ao buscar imagens" }, { status: 500 });
+  }
 }
