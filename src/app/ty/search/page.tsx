cdec58be70df9512dfc34c807081fb2e812c6697
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-09 21:48:58 -0300

    feat: changed search page

--- src/app/ty/search/page.tsx
+++ src/app/ty/search/page.tsx
@@ -2,29 +2,19 @@
 import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
-import ImageResults from "@/app/components/ImageResults";
 import Header from "@/app/components/Header";
 import Nav from "@/app/components/Nav";
 import Footer from "@/app/components/Footer";
 
-import { Wind } from "./styles";
-
 
 export default function SearchPage() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("query") || "nature";
-
   return (
     <>
       <Header />
       <Nav />
-      <Wind>
-        <h2>Resultados para: &quot;<span>{query}</span>&quot;</h2>
-        <div className="mainWin">
-          <ImageResults query={query} />
-
-        </div>
-      </Wind>
+      <Suspense fallback={<div>Carregando busca...</div>}>
+        <SearchContent />
+      </Suspense>
       <Footer />
     </>
   );
