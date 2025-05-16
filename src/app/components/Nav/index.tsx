4d01f9bb8f9cc025df56e6bd3b6cbc8ad88a60c5
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-16 00:36:59 -0300

    feat: add className .active

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -9,19 +9,17 @@ export default function Nav() {
   return (
     <NavBar>
       <Link href={"/ty/foto"} className={clsx({ active: path === "/ty/foto" })}>Foto</Link>
-      <Link href={"../../ty/wallpapers"}>Wallpaper</Link>
-      <Link href={"../../ty/textura"}>Textura</Link>
-      <Link href={"../../ty/universo"}>Universo</Link>
-      <Link href={"../../ty/natureza"}>Natureza</Link>
-      <Link href={"../../ty/carro"}>Carro</Link>
-      <Link href={"../../ty/moto"}>Moto</Link>
-      <Link href={"../../ty/animais"}>Animais</Link>
-      <Link href={"../../ty/rua"}>Rua</Link>
-      <Link href={"../../ty/viagem"}>Viagem</Link>
-      <Link href={"../../ty/flor"}>Flor</Link>
-      <Link href={"../../ty/moda"}>Moda</Link>
-      
-
+      <Link href={"/ty/wallpapers"} className={clsx({ active: path === "/ty/wallpapers" })}>Wallpaper</Link>
+      <Link href={"/ty/textura"} className={clsx({ active: path === "/ty/textura" })}>Textura</Link>
+      <Link href={"/ty/universo"} className={clsx({ active: path === "/ty/universo" })}>Universo</Link>
+      <Link href={"/ty/natureza"} className={clsx({ active: path === "/ty/natureza" })}>Natureza</Link>
+      <Link href={"/ty/carro"} className={clsx({ active: path === "/ty/carro" })}>Carro</Link>
+      <Link href={"/ty/moto"} className={clsx({ active: path === "/ty/moto" })}>Moto</Link>
+      <Link href={"/ty/animais"} className={clsx({ active: path === "/ty/animais" })}>Animais</Link>
+      <Link href={"/ty/rua"} className={clsx({ active: path === "/ty/rua" })}>Rua</Link>
+      <Link href={"/ty/viagem"} className={clsx({ active: path === "/ty/viajem" })}>Viagem</Link>
+      <Link href={"/ty/flor"} className={clsx({ active: path === "/ty/flor" })}>Flor</Link>
+      <Link href={"/ty/moda"} className={clsx({ active: path === "/ty/moda" })}>Moda</Link>
     </NavBar>
   )
 }
